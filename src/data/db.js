d81dfbf5b880d5281ff615f4f643b97661600f38
Mark Honeychurch <mark@honeychurch.org>
2022-07-03 22:11:55 +1200

    Add default name for placenames

--- src/data/db.js
+++ src/data/db.js
@@ -1,12 +1,25 @@
 const {join} = require('path');
 const {readdirSync} = require('fs');
 
-module.exports = async (cached = true, cache = true, path = join(__dirname, 'json')) => {
+function linkName(collection) {
+	collection.forEach((placename) => {
+		placename.name = placename.names[0].name;
+	});
+}
+
+module.exports = async (addname = false, cached = true, cache = true, path = join(__dirname, 'json')) => {
+	let db;
 	if (cached) {
-		return readdirSync(path).map((file) => file.endsWith('.json') && file.replace('.json', '')).filter(Boolean).reduce((db, collection) => {
+		db = readdirSync(path).map((file) => file.endsWith('.json') && file.replace('.json', '')).filter(Boolean).reduce((db, collection) => {
 			db[collection] = require(join(path, collection + '.json'));
+			if (addname && collection === 'placenames') {
+				linkName(db.placenames);
+			}
+			return db;
 		}, {});
 	} else {
-		return await connectData(await importData(cache), cache);
+		db = connectData(await importData(cache), cache);
+		linkName(db.placenames);
 	}
+	return db;
 };
