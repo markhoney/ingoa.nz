44418e1006388749939183df6c736b25f2059897
Mark Honeychurch <mark@honeychurch.org>
2022-07-03 13:50:24 +1200

    Remove unneeded files

--- src/routes/static.js
+++
@@ -1,8 +0,0 @@
-
-async function getRoutes() {
-	//console.log(require('./dynamic'));
-	const routes = await(require('./dynamic'));
-	//console.log(routes);
-}
-
-getRoutes();
--- src/server/apollo/apollo.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	service: {
-		localSchemaFile: './src/server/apollo/schema.graphql',
-		endpoint: {
-			url: "http://localhost:4000"
-		},
-	}
-};
