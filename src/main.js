4aeb34db31a67d765781a7d82945ed774491ecaf
Mark Honeychurch <mark@honeychurch.org>
2022-07-05 01:23:59 +1200

    Add helpers to Vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import BootstrapVue from 'bootstrap-vue';
+import pluralise from 'pluralize';
+import * as changeCase from 'change-case';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import query from '@/plugins/query';
+import locale from '@/plugins/locale';
 
-import {cleanPage} from '@/data/utils';
-
-export default function (Vue, {head, appOptions}) {
+export default function (Vue, {appOptions, head}) {
 	Vue.use(BootstrapVue);
-	Vue.prototype.$clean = cleanPage;
+	Vue.prototype.$tf = locale(appOptions.i18n);
+	Vue.prototype.$q = query;
+	Vue.prototype.$case = changeCase;
+	Vue.prototype.$pluralise = pluralise;
 	head.htmlAttrs = {class: 'h-100'};
 	head.bodyAttrs = {class: 'd-flex flex-column h-100'};
 }
