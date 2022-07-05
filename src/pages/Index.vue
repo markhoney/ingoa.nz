0c4035f95ffa5e7a893e3b92e8285724bb345b16
Mark Honeychurch <mark@honeychurch.org>
2022-07-06 00:13:23 +1200

    Added text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,5 +2,10 @@
 	<article>
 		<h1>{{$t('title')}}</h1>
 		<h2>{{$t('subtitle')}}</h2>
+		<p class="m-3" style="font-style: italic;" v-html="$t('aboutText')" />
+		<p class="m-3" v-html="$t('introText')" />
+		<p class="m-3" v-html="$t('instructionsText')" />
+		<b-alert show variant="warning" v-html="$t('disclaimerText')">
+		</b-alert>
 	</article>
 </template>
