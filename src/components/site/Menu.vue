e99d80dda26dec5d251ad6d120148609de9c848a
Mark Honeychurch <mark@honeychurch.org>
2022-07-05 23:24:55 +1200

    Fix menu i18n

--- src/components/site/Menu.vue
+++ src/components/site/Menu.vue
@@ -1,14 +1,18 @@
 <template>
 		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark w-100">
 			<div class="d-flex w-100 justify-content-between">
-				<a class="navbar-brand" href="/">{{$t('title')}}</a>
+				<g-link class="navbar-brand" :to="$tp('/')">{{$t('title')}}</g-link>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarCollapse">
 					<ul class="navbar-nav mb-2 mb-md-0">
-						<li class="nav-item"><g-link class="nav-link" to="/">Home</g-link></li>
-						<li v-for="item in items" :key="item" class="nav-item"><g-link class="nav-link" :to="$tp('/' + $tc(item, 2))">{{$case.sentenceCase($tc(item, 2))}}</g-link></li>
+						<li class="nav-item"><g-link class="nav-link" :to="$tp('/')">{{$case.sentenceCase($t('home'))}}</g-link></li>
+						<li v-for="type in types" :key="type" class="nav-item">
+							<g-link class="nav-link" :to="$tp(`/${$tc('type.' + type, 2)}/`)">
+								{{$case.sentenceCase($tc('type.' + type, 2))}}
+							</g-link>
+						</li>
 					</ul>
 				</div>
 				<search class="m-2" />
@@ -24,7 +28,7 @@
 		components: {Search},
 		data() {
 			return {
-				items: ['island', 'part', 'map', 'region', 'sector', 'district', 'zone', 'group', 'feature', 'iwi', 'placenames', 'speaker'],
+				types: ['island', 'part', 'map', 'region', 'sector', 'district', 'zone', 'group', 'feature', 'iwi', 'placename', 'speaker'],
 			};
 		},
 	};
