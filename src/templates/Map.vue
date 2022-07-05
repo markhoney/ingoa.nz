710d770b3a4fe69b34903710ff5dd94c82ebe1b9
Mark Honeychurch <mark@honeychurch.org>
2022-07-05 23:25:11 +1200

    Add map template

---
+++ src/templates/Map.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<h1>{{$page.item.name.locale.en}} ({{$page.item.name.locale.mi}})</h1>
+	</div>
+</template>
+
+<page-query>
+	query item($id: ID!) {
+		item: map(id: $id) {
+			id
+			name {
+				locale {
+					en
+					mi
+				}
+			}
+		}
+	}
+</page-query>
+
+<script>
+	export default {
+		metaInfo () {
+			return {
+				title: this.$page.item.name.locale.en,
+			}
+		},
+	};
+</script>
